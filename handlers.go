916800242c0d38bf20705fbcb80696456d1c107e
ilackarms <sdw35@cornell.edu>
2018-02-18 13:13:55 -0500

    add new type of handler

--- handlers.go
+++ handlers.go
@@ -1,6 +1,9 @@
 package kubecontroller
 
-import "k8s.io/client-go/tools/cache"
+import (
+	"k8s.io/client-go/tools/cache"
+	"sync"
+)
 
 // returns a handler that runs f() every time an update occurs,
 // regardless of which type of update
@@ -14,6 +17,30 @@ func NewSyncHandler(f func()) cache.ResourceEventHandler {
 		},
 		DeleteFunc: func(obj interface{}) {
 			f()
+		},
+	}
+}
+
+// returns a handler that runs f() every time an update occurs,
+// regardless of which type of update
+// ensures only one f() can run at a time
+func NewLockingSyncHandler(f func()) cache.ResourceEventHandler {
+	var mu sync.Mutex
+	return &cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			mu.Lock()
+			f()
+			mu.Unlock()
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			mu.Lock()
+			f()
+			mu.Unlock()
+		},
+		DeleteFunc: func(obj interface{}) {
+			mu.Lock()
+			f()
+			mu.Unlock()
 		},
 	}
 }
